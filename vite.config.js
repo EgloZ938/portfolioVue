9d5e741a86f817954cc2dc08a8e5a59c39daf687
EgloZ938 <theo.evon.2004@gmail.com>
2025-01-27 00:26:32 +0100

    Update vite config for production

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+const plugins = [vue()]
+
+// Ajout des devtools uniquement en développement
+if (process.env.NODE_ENV === 'development') {
+  const vueDevTools = await import('vite-plugin-vue-devtools')
+  plugins.push(vueDevTools.default())
+}
+
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins,
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
